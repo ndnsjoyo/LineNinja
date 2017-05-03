0e3d3aaccefdd5f09a63e200bde9a53896c047d4
wmmmm <1229819460@qq.com>
2017-05-04 01:59:10 +0800

    竹子替换
    1.圆柱体换竹子
    2.生成和消失时竹子的动画
    3。碰到竹子时的方向改变

---
+++ Assets/Scripts/BamAnimOnCapsule.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamAnimOnCapsule : MonoBehaviour {
+    public List<GameObject> bamboos;
+	// Use this for initialization
+    void Awake()
+    {
+        bamboos = new List<GameObject>();
+    }
+	void Start () {
+		
+	}
+    //void OnCollisionEnter(Collision other)
+    //{
+    //    float playerDir = other.gameObject.transform.localEulerAngles.y;
+
+    //    float selfDir = transform.localEulerAngles.y;
+
+
+
+    //    if ((selfDir - playerDir) > 90 && (selfDir - playerDir) < 270)
+    //    {
+    //        foreach (GameObject bam in bamboos)
+    //        {
+    //            bam.GetComponent<Animator>().SetTrigger("right");
+    //        }
+    //        print("右");
+    //    }
+    //    else
+    //    {
+    //        foreach (GameObject bam in bamboos)
+    //        {
+    //            bam.GetComponent<Animator>().SetTrigger("left");
+    //        }
+    //        print("左");
+    //    }
+    //    //foreach (GameObject bam in bamboos)
+    //    //{
+    //    //    bam.GetComponent<Animator>().SetInteger("right", 0);
+    //    //}
+    //}
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/BambooAnimation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class BambooAnimation : MonoBehaviour {
+    public int index = 0;
+	// Use this for initialization
+	void Start () {
+        posInit();
+        StartCoroutine(Burn(index));
+
+       
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player") return;
+
+        float playerDir = other.gameObject.transform.localEulerAngles.y;
+        float selfDir = transform.localEulerAngles.y;
+
+  
+
+        if ((selfDir - playerDir) > 0 && (selfDir - playerDir) < 180)
+        {
+          
+               GetComponent<Animator>().SetTrigger("left");
+            print("left");
+        }
+        else
+        {
+           
+                GetComponent<Animator>().SetTrigger("right");
+            print("right");
+
+        }
+        //foreach (GameObject bam in bamboos)
+        //{
+        //    bam.GetComponent<Animator>().SetInteger("right", 0);
+        //}
+    }
+
+    public void Death()
+    {
+        Tweener tween = transform.DOMoveY(-3, 0.1f);
+        tween.SetEase(Ease.Linear);
+        
+    }
+    
+
+
+    void posInit()
+    {
+        Vector3 pos = transform.localPosition;
+        transform.localPosition = new Vector3(pos.x, pos.y - 3, pos.z);
+    }
+
+
+    IEnumerator  Burn(int index)
+    {
+        yield return new WaitForSeconds(index * 0.01f);
+      
+
+        Tweener tween= transform.DOMoveY(0, 0.1f);
+        tween.SetEase(Ease.Linear);
+    }
+}
