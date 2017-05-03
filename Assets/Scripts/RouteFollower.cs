418da83d064758f23f7f23e484e030d47918ca9a
Andrew Huang <andyroiiid@gmail.com>
2017-05-03 16:30:50 +0800

    包装spline move

---
+++ Assets/Scripts/RouteFollower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SWS;
+using DG.Tweening;
+
+[RequireComponent(typeof(splineMove))]
+public class RouteFollower : MonoBehaviour
+{
+    private splineMove _splineMove;
+
+    public float speed = 5;
+    public bool lockRotation = true;
+    public float waitTime = 1.0f;
+
+    void Start()
+    {
+        _splineMove = GetComponent<splineMove>();
+
+        _splineMove.onStart = true;
+        _splineMove.moveToPath = true;
+        _splineMove.loopType = splineMove.LoopType.pingPong;
+        _splineMove.pathType = PathType.Linear;
+
+        _splineMove.speed = speed;
+        if (lockRotation)
+            _splineMove.lockRotation = AxisConstraint.Y;
+
+        _splineMove.events[0]
+            .AddListener(() => _splineMove.Pause(waitTime));
+        _splineMove.events[_splineMove.events.Count - 1]
+            .AddListener(() => _splineMove.Pause(waitTime));
+    }
+}
