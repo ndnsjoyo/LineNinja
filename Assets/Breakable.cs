e2a3eeb421e178067caf10f7160d2e91fa320264
Andrew Huang <andyroiiid@gmail.com>
2017-05-03 17:32:45 +0800

    试着用粒子效果做怪物死亡

---
+++ Assets/Breakable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public GameObject broken;
+
+    void OnDestroy()
+    {
+        GameObject.Instantiate(broken, transform.position, transform.rotation);
+    }
+}
